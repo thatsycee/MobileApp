d6ff4d418e0c01cfe59da8246de8b0753d4a0fd4
thatsycee <juan.patreece@gmail.com>
2026-05-01 20:12:19 +0800

    Update index.tsx (05/01/2026)
    Laboratory 2: Create your own Profile Card Screen Submission

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,12 +1,42 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View, Text} from 'react-native';
 
+/*
 import { HelloWave } from '@/components/hello-wave';
+*/
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
+import { BounceIn } from 'react-native-reanimated';
 
+
+export default function HomeScreen() {
+  return (
+    <View style={s.screen}>
+      <Image
+        source={{ uri: 'https://scontent.fmnl4-8.fna.fbcdn.net/v/t39.30808-6/504248074_24796994163223795_812235706363061218_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeEfgCJWMpPz2VeAzlEEBvJvDs60j55VouwOzrSPnlWi7A_7szePy2xjFDhsXmyI4LFfS_pnk2TQtFVpUGsVm136&_nc_ohc=Vn0jZkQaz-oQ7kNvwGQbVam&_nc_oc=Adr1GfIK8mnTHD1TozG7S7BmtXf-Ju07ow4lwYmnXJzdVmCErkymc0MLZTPl1Uso4a0&_nc_zt=23&_nc_ht=scontent.fmnl4-8.fna&_nc_gid=rW6e9gFCX1BGcA74iCAc6Q&_nc_ss=7b2a8&oh=00_Af4N0WV1srZYJZeDhGwY0TrIVZ4KU8kQfQBseubYUQm5iA&oe=69FA6E2D' }}
+        style={s.photo}
+      />
+      <Text style={s.name}>Patreece Monique U. Juan</Text>
+      <Text style={s.bio}>Multimedia Arts Student | Social Media Content Specialist</Text>
+    </View>
+  );
+}
+
+// I change the background color, added a border and shadow. 05/01/2026
+const s = StyleSheet.create({
+  screen: { flex:1, alignItems: 'center', justifyContent:'center', backgroundColor: '#bcde7d' },
+  photo:  { width:120, height:120, borderRadius:60, borderWidth:5, borderColor: '#f5a3dd', shadowColor: '#a74871', shadowOffset: { width: 3, height: 6 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 6 },
+  name:   { fontSize:22, fontWeight:'bold', marginTop:12 },
+  bio:    { fontSize:14, color:'#888888' }
+});
+
+/*
 export default function HomeScreen() {
 
   const myName = "Reeze";
@@ -89,3 +119,4 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+*/
